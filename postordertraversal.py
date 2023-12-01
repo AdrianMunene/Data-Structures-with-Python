c8711bde31c0622a0fb36a0f69764509840e19f8
AdrianMunene <adrianmunene62@gmail.com>
2023-12-01 20:14:32 +0300

    Dijkstra's Algorithm

--- postordertraversal.py
+++ postordertraversal.py
@@ -5,15 +5,61 @@ class TreeNode(object):
         self.right = right
 
 
-def postorderTraversal(root):
-    if root == None:
-        return 
+# def postorderTraversal(root):
+#     if root == None:
+#         return 
 
-    postorderTraversal(root.left)
+#     postorderTraversal(root.left)
 
-    postorderTraversal(root.right)
+#     postorderTraversal(root.right)
 
-    print(root.val)
+#     print(root.val)
+def peek(stack): 
+    if len(stack) > 0: 
+        return stack[-1] 
+    return None
+
+    
+def postOrderIterative(root): 
+         
+    # Check for empty tree 
+    if root is None: 
+        return
+ 
+    stack = [] 
+     
+    while(True): 
+         
+        while (root): 
+            # Push root's right child and then root to stack 
+            if root.right is not None: 
+                stack.append(root.right) 
+            stack.append(root) 
+ 
+            # Set root as root's left child 
+            root = root.left 
+         
+        # Pop an item from stack and set it as root 
+        root = stack.pop() 
+ 
+        # If the popped item has a right child and the 
+        # right child is not processed yet, then make sure 
+        # right child is processed before root 
+        if (root.right is not None and
+            peek(stack) == root.right): 
+            stack.pop() # Remove right child from stack 
+            stack.append(root) # Push root back to stack 
+            root = root.right # change root so that the 
+                            # right childis processed next 
+ 
+        # Else print root's data and set root as None 
+        else: 
+            ans.append(root.data) 
+            root = None
+ 
+        if (len(stack) <= 0): 
+                break
+
 
 root = TreeNode()
 a1 = TreeNode()
